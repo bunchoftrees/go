03c7e52a23638d31ee15644834048c99807b09ba
Forrest Hartley <forrest@forrestbear.org>
2020-05-13 17:51:39 -0400

    removed TODOs

--- syntax/packaging/examples.go
+++ syntax/packaging/examples.go
@@ -14,7 +14,6 @@ func main() {
 	fmt.Printf("My circle has a radius of %v units.\n", myCircle.Radius)
 	fmt.Printf("The area this circle is %v square units.\n", myCircle.Area())
 	fmt.Printf("The circumference this circle is %v units.\n", myCircle.Circumference())
-	// TODO: Implement getter for radius
 
 	// My Rectangle
 	myRectangle := shapes.NewRectangle(3, 4)
@@ -23,5 +22,4 @@ func main() {
 	fmt.Printf("The area of this rectangle is %v square units.\n", myRectangle.Area())
 	fmt.Printf("The perimeter of this rectangle is %v units.\n", myRectangle.Perimeter())
 	fmt.Printf("The diagonal of this rectangle has a length of %v units.\n", myRectangle.Diagonal())
-	// TODO: Implement getter for width and length
 }
